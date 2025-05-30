1d9d411ef4cfb22a3e031e7ad3d60d1793cc60f1
Azlan-saja <azlansaja19@gmail.com>
2025-05-30 16:04:59 +0700

    uas selesai

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,4 @@
 class DataSaya {
-   static String nama = "Kudut";
-    static String gambar = "assets/mahasiswa/kudut.png";
+  static String nama = "Kudut Mehra S2";
+  static String gambar = "assets/mahasiswa/kudut.png";
 }
